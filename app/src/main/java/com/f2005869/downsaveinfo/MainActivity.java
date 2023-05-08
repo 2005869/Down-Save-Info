200955620c0180eb2cccba339d40bc7a6bfa0556
2005869 <2005869@aluno.univesp.br>
2023-05-08 16:33:57 -0300

    edit main screen and screen2 and insert logic to send text to next screen

--- app/src/main/java/com/f2005869/downsaveinfo/MainActivity.java
+++ app/src/main/java/com/f2005869/downsaveinfo/MainActivity.java
@@ -2,13 +2,40 @@ package com.f2005869.downsaveinfo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.EditText;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Receive an input for the user
+    private EditText insertedMessage;
+
+    //message to send
+    private String message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //connect variable insertMessage with EditText in xml
+        insertedMessage = findViewById(R.id.editTextXML);
+    }
+
+    //method to send message to next screen
+    public void nextScreen(View v){
+        //attr insertMessage to message and convert to string
+        message = insertedMessage.getText().toString();
+
+        //create an Intent to send message to next screen
+        Intent myIntent = new Intent(this, Screen2.class);
+
+        //use method putExtra to send the message
+        myIntent.putExtra(name="sentMessage", message);
+
+        //open next screen
+        startActivity(myIntent);
     }
 }
---
+++ app/src/main/java/com/f2005869/downsaveinfo/Screen2.java
@@ -0,0 +1,14 @@
+package com.f2005869.downsaveinfo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Screen2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen2);
+    }
+}
